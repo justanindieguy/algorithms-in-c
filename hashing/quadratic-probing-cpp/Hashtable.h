0494867ba3ac18eede87b7f0dc44ed4745d68ba0
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2021-12-21 20:39:43 -0600

    fix wrong function declaration in Hashtable.h for quadratic-probing-cpp

--- hashing/quadratic-probing-cpp/Hashtable.h
+++ hashing/quadratic-probing-cpp/Hashtable.h
@@ -1,6 +1,5 @@
-#ifndef Hashtable_h
-#define Hashtable_h
-#define SIZE 10
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
 
 class HashTable
 {
@@ -13,46 +12,5 @@ private:
     int hash(int key);
     int quadraticProbe(int key);
 };
-
-int HashTable::hash(int key)
-{
-    return key % SIZE;
-}
-
-int HashTable::quadraticProbe(int key)
-{
-    int idx = hash(key);
-    int i = 0;
-
-    while (ht[(idx + i * i) % SIZE] != 0)
-        i++;
-
-    return (idx + i * i) % SIZE;
-}
-
-void HashTable::insert(int key)
-{
-    int idx = hash(key);
-
-    if (ht[idx] != 0)
-        idx = quadraticProbe(idx);
-
-    ht[idx] = key;
-}
-
-int HashTable::search(int key)
-{
-    int idx = hash(key);
-    int i = 0;
-
-    while (ht[(idx + i * i) % SIZE] != key)
-    {
-        i++;
-        if (ht[(idx + i * i) % SIZE] == 0)
-            return -1;
-    }
-
-    return (idx + i * i) % SIZE;
-}
 
 #endif
